3fff4568c4923d2899a454621937db4790b9900e
하빈 <habinkim0427@naver.com>
2025-11-30 20:10:27 +0900

    다크모드, 라이트모드 추가 및 애니메이션 수정

--- js/main.js
+++ js/main.js
@@ -1,29 +1,43 @@
-const root = document.documentElement;
-
-function toggleTheme() {
-  root.setAttribute(
-    "data-theme",
-    root.getAttribute("data-theme") === "dark" ? "light" : "dark"
-  );
-}
-
-document.querySelector(".theme-button").addEventListener("click", toggleTheme);
+let mainEffectAction = false;
 
 function mainEffect() {
-  const profileBox = document.querySelector(".hero");
+  // hero 이벤트 등록
+  const hero = document.querySelector(".hero");
+  const aboutMeBtn = document.querySelector(".menu-title.aboutme");
 
-  // PC hover
-  profileBox.addEventListener("mouseenter", () => {
-    profileBox.classList.add("active");
+  aboutMeBtn.addEventListener("mouseenter", () => {
+    hero.classList.add("active");
+  });
+  aboutMeBtn.addEventListener("click", () => {
+    hero.classList.add("active");
   });
 
-  profileBox.addEventListener("mouseleave", () => {
-    profileBox.classList.remove("active");
+  hero.addEventListener("mouseenter", () => {
+    hero.classList.add("active");
+  });
+  hero.addEventListener("mouseleave", () => {
+    hero.classList.remove("active");
+  });
+  hero.addEventListener("click", () => {
+    hero.classList.add("active");
   });
 
-  // 모바일 click toggle
-  profileBox.addEventListener("click", () => {
-    profileBox.classList.toggle("active");
-  });
+  // 실행 확인 플래그 true로 변경
+  mainEffectAction = true;
 }
-mainEffect();
+
+mainEffect(); // 실행해야 아래 이벤트가 유효해짐
+
+// 실행 됐을 때만 작동할 이벤트
+const box = document.querySelector(".hero");
+const profileTxtBox = document.querySelector(".profile-txt-box");
+const aboutMeBtn = document.querySelector(".menu-title.aboutme");
+
+box.addEventListener("click", () => {
+  if (!mainEffectAction) return;
+  profileTxtBox.classList.toggle("top");
+});
+
+aboutMeBtn.addEventListener("click", () => {
+  profileTxtBox.classList.toggle("top");
+});
