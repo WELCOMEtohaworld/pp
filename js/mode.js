74d2756d66d0cc9fae5e72d4078fe78aab57825c
하빈 <habinkim0427@naver.com>
2025-12-08 15:54:52 +0900

    기본 폰트 추가 및 모바일 가시화면 초기세팅

--- js/mode.js
+++ js/mode.js
@@ -1,12 +1,21 @@
+// const root = document.documentElement;
+// const modeBtn = document.querySelector(".modeBtn");
+// const modeIcon = document.querySelector(".modeBtn > img");
+
+// function toggleTheme() {
+//   const darkMode = root.getAttribute("data-theme") === "dark";
+//   root.setAttribute("data-theme", darkMode ? "light" : "dark");
+
+//   modeIcon.src = darkMode ? "img/light_border.png" : "img/dark.png";
+// }
+
+// modeBtn.addEventListener("click", toggleTheme);
 const root = document.documentElement;
 const modeBtn = document.querySelector(".modeBtn");
-const modeIcon = document.querySelector(".modeBtn > img");
 
 function toggleTheme() {
   const darkMode = root.getAttribute("data-theme") === "dark";
   root.setAttribute("data-theme", darkMode ? "light" : "dark");
-
-  modeIcon.src = darkMode ? "img/light_border.png" : "img/dark.png";
 }
 
 modeBtn.addEventListener("click", toggleTheme);
